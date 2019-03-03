8894616fdf4b6a165f7288d23bb748bd49a7129f
MichaelKolosovskiy <47351272+MichaelKolosovskiy@users.noreply.github.com>
2019-03-03 19:57:53 +0200

    Update HomeworkApp.java

--- HW3/src/cardetails/HomeworkApp.java
+++ HW3/src/cardetails/HomeworkApp.java
@@ -3,28 +3,28 @@ package cardetails;
 import java.time.LocalDate;
 
 public class HomeworkApp {
- 
-	public static void main(String[] args) {
-		CarDoor car = new CarDoor(true,true);
-		car.closeDoor();
-		car.closeWindow();
-		System.out.println(car.toString());
-		System.out.println();
-		
-		CarWheel  w = new CarWheel();
-		w.tireWear(1);
-		w.changeCarWheelOrNot();
-		System.out.println();
-		 
-		Car car1 = new Car("Fuel",300,100, 4, -234,123, LocalDate.now());
-		System.out.println(car1.maxCurrentSpeed());
-		car1.changeCurrentSpeed(43);
-		System.out.println(car1.toString());
-		System.out.println();
-		car1.changeCurrentSpeed(76);
-		System.out.println(car1.maxCurrentSpeed());
-		System.out.println(car1.toString());
 
-	}
+    public static void main(String[] args) {
+        CarDoor car = new CarDoor(true, true);
+        car.closeDoor();
+        car.closeWindow();
+        System.out.println(car.toString());
+        System.out.println();
+
+        CarWheel w = new CarWheel();
+        w.tireWear(1);
+        w.changeCarWheelOrNot();
+        System.out.println();
+
+        Car car1 = new Car("Fuel", 300, 100, 4, -234, 123, LocalDate.now());
+        System.out.println(car1.maxCurrentSpeed());
+        car1.changeCurrentSpeed(43);
+        System.out.println(car1.toString());
+        System.out.println();
+        car1.changeCurrentSpeed(76);
+        System.out.println(car1.maxCurrentSpeed());
+        System.out.println(car1.toString());
+
+    }
 
 }
