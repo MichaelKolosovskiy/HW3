b492f0cd35ca968e1ef0b200d2e4cceef22688b8
MichaelKolosovskiy <47351272+MichaelKolosovskiy@users.noreply.github.com>
2019-03-03 19:56:43 +0200

    Update CarDoor.java

--- HW3/src/cardetails/CarDoor.java
+++ HW3/src/cardetails/CarDoor.java
@@ -1,31 +1,49 @@
 package cardetails;
 
-public class CarWheel {
+public class CarDoor {
 
-    private double amountOfTheWheel;
+    private boolean carDoor;
+    private boolean carWindow;
 
-    public CarWheel() {
-        this.amountOfTheWheel = Math.random();
+    public CarDoor() {
+        this.carDoor = false;
+        this.carWindow = false;
     }
 
-    public CarWheel(double amountOfTheWheel) {
-        this.amountOfTheWheel = amountOfTheWheel;
+    public CarDoor(boolean carDoor, boolean carWindow) {
+        this.carDoor = carDoor;
+        this.carWindow = carWindow;
     }
 
-    public void tireWear(int percent) {
-        this.amountOfTheWheel = amountOfTheWheel - (0.01 * percent);
+    public void openDoor() {
+        this.carDoor = true;
     }
 
-    public double getStateOfTheWheel() {
-        return this.amountOfTheWheel;
+    public void closeDoor() {
+        this.carDoor = false;
     }
 
-    public void changeCarWheelOrNot() {
-        if (this.amountOfTheWheel >= 0 && this.amountOfTheWheel <= 0.30) {
-            System.out.println("You need to change your wheels");
-        } else {
-            System.out.println("You don't need to change your wheels");
-        }
+    public void openWindow() {
+        this.carWindow = true;
     }
+
+    public void closeWindow() {
+        this.carWindow = false;
+    }
+
+    public boolean ifDoorIsClosedOrOpened() {
+        return (this.carDoor == false) ? true : false;
+    }
+
+    public boolean ifWindowIsClosedOrOpened() {
+        return (this.carWindow == false) ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Is the door open ? " + this.carDoor
+                        + "\nIs the window open ? " + this.carWindow;
+    }
+
 }
-
